8836cbfa5c59c57dc14e472dc8059a224fa38fd9
Soremekun Oluwatosin <soremekunoluwatosinsamuel@gmail.com>
2018-04-20 16:44:13 +0100

    We can now select between apache and nginx

--- src/NewSiteCommand.php
+++ src/NewSiteCommand.php
@@ -1,5 +1,6 @@
 <?php namespace therealsmat;
 
+use Symfony\Component\Console\Input\ArrayInput;
 use Symfony\Component\Console\Input\Input;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
@@ -12,12 +13,38 @@ class NewSiteCommand extends CommandStructure {
     {
         $this->setName('new:site')
             ->setDescription('Creates a new virtual host configuration for a new site')
-            ->addOption('nginx', null, InputOption::VALUE_OPTIONAL, 'Your server', true);
+            ->addOption('server', null, InputOption::VALUE_OPTIONAL | InputOption::VALUE_IS_ARRAY, 'Your server', ['nginx', 'apache']);
     }
 
+    /**
+     * @param InputInterface $input
+     * @param OutputInterface $output
+     * @return int|null
+     * @throws \Exception
+     */
     public function execute(InputInterface $input, OutputInterface $output)
     {
-        $option = $input->getParameterOption('--nginx');
-        $output->write($option);
+        $option = $input->getParameterOption('--server');
+        if ($option == 'nginx') {
+            $command = $this->getApplication()->find('new:nginx-site');
+            $args = [
+                'command' => 'new:nginx-site'
+            ];
+            $commandInput = new ArrayInput($args);
+            $output->writeln("============================================");
+            $output->writeln("Nginx Server Block Wizard");
+            $output->writeln("============================================");
+            return $command->run($commandInput, $output);
+        } else {
+            $command = $this->getApplication()->find('new:apache-site');
+            $args = [
+                'command' => 'new:apache-site'
+            ];
+            $commandInput = new ArrayInput($args);
+            $output->writeln("============================================");
+            $output->writeln("Apache Virtual Host Wizard");
+            $output->writeln("============================================");
+            return $command->run($commandInput, $output);
+        }
     }
 }
