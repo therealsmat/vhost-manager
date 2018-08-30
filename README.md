b372624e7fd238c876b76df8c7a4ae35ee4f800e
Soremekun Oluwatosin <soremekunoluwatosinsamuel@gmail.com>
2018-08-30 03:56:49 +0100

    work in progress

--- README.md
+++ README.md
@@ -11,24 +11,27 @@ Download the tool using composer
 
 `composer global require therealsmat/vhost-manager`
 
+After downloading, 
+
 ## Usage
 To list all available sites, run `vhost sites`
 
 To create a new site, run `vhost new:site`. You will be prompted to
 
 - Enter the site domain name. This is the url you want to use to access your app e.g `site1.local`
+- Enter the document root. This is the path tp your project.
 - Enter a public directory. This is the directory inside which you want your apllication to be served from.
 
 Do not confuse the public directory with the root directory. By default, the directory you are running the command from will be used as the default root directory. The public directory is the directory you want web requests to be served from. e.g `site/public`. Here, our root directory is `site` since it will contain all of our application code. However, we want all requests to be served from the `public` directory, so `public` will be our public directory.
 
+To remove a site, run `vhost delete:site`
+
 ## Todo's
 These features will be shipping soon;
 
-- Rename site
 - Change public directory
 - Use a different root directory
 - Serve site on a different port
-- Delete a site (vhost)
 
 ## License
 MIT
--- src/DeleteSiteCommand.php
+++ src/DeleteSiteCommand.php
@@ -7,12 +7,34 @@ use Symfony\Component\Console\Question\ConfirmationQuestion;
 
 class DeleteSiteCommand extends CommandStructure {
 
+    /**
+     * Available sites directory
+     * @var string
+     */
+    protected $sites_available_dir = '/etc/apache2/sites-available/';
+
+    /**
+     * Enabled site directory
+     * @var string
+     */
+    protected $sites_enabled_dir = '/etc/apache2/sites-enabled/';
+
+    /**
+     * Configure the command
+     * @return [type] [description]
+     */
     public function configure()
     {
         $this->setName('delete:site')
             ->setDescription('Deletes an existing virtual host site');
     }
 
+    /**
+     * Execute the command
+     * @param  InputInterface  $input  [description]
+     * @param  OutputInterface $output [description]
+     * @return [type]                  [description]
+     */
     public function execute(InputInterface $input, OutputInterface $output)
     {
         $options = $this->availableSitesArray();
@@ -36,12 +58,21 @@ class DeleteSiteCommand extends CommandStructure {
 
     }
 
+    /**
+     * Get a list of the available sites
+     * @return [type] [description]
+     */
     private function availableSitesArray()
     {
         $sites = $this->getAvailableSites();
         return explode(PHP_EOL, $sites);
     }
 
+    /**
+     * Delete a virtual host
+     * @param  [type] $site [description]
+     * @return [type]       [description]
+     */
     private function deleteSite($site)
     {
         $this->runCommand("rm {$this->sites_enabled_dir}/{$site}");
